a8ebdbe7336a0c5db8887910cb991aba1bd84b2c
Subhashiny Ganeshan <43635284+subhashinyg@users.noreply.github.com>
2022-05-20 13:22:08 +0530

    static files added

--- be_connected/settings.py
+++ be_connected/settings.py
@@ -97,7 +97,7 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'NAME': 'database',
         'USER': 'postgres',
         'PASSWORD':'postgres123',
         'HOST':'database.coy2xbfytt2o.ap-south-1.rds.amazonaws.com'
@@ -143,6 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- category_app/api/views.py
+++ category_app/api/views.py
@@ -12,7 +12,11 @@ from django.core import serializers
 from rest_framework import viewsets
 
 # Create your views here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
+class User(AbstractUser):
+  pass
 
 class CategoryView(APIView):
     def get(self, request):

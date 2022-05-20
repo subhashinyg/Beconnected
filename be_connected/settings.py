3cb7e4906cc2ac11cd73029f8e08b2f677e7215c
Subhashiny Ganeshan <43635284+subhashinyg@users.noreply.github.com>
2022-05-20 13:40:26 +0530

    db

--- be_connected/settings.py
+++ be_connected/settings.py
@@ -97,7 +97,7 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'database',
+        'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD':'postgres123',
         'HOST':'database.coy2xbfytt2o.ap-south-1.rds.amazonaws.com'
@@ -143,7 +143,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_URL = 'static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

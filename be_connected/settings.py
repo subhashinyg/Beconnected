fa40187e99a811320fba644c212cd1d4d36538d5
Subhashiny Ganeshan <43635284+subhashinyg@users.noreply.github.com>
2022-05-29 23:27:18 +0530

    static files

--- be_connected/settings.py
+++ be_connected/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,6 +146,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATIC_URL = 'static'
+STATIC_ROOT = '/static/'
+
 
 
 # Default primary key field type
